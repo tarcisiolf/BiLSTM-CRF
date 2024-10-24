42626c4393175280998ca990a28b4fe6e48f992f
Tarcísio Lima Ferreira <tarcisiolf0@gmail.com>
2024-10-24 17:56:46 -0300

    remove: old files

--- ignore_files/requeriments.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-import tensorflow_addons as tfa
-import sklearn
-import pandas as pd
-
-print("Tensorflow Version", tf. __version__)
-print("Tensorflow Addons Version", tfa. __version__)
-print("Sklearn Version", sklearn.__version__)
-print("Pandas Version", pd.__version__)
